c3b42e73775be81fe157814998652524f6e4ecf7
LongXuanBui <xuanlong1372000@gmail.com>
2022-07-15 08:26:20 +0700

    firs_commit

---
+++ gpio_output.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "gpio_output.h"
+gpio_config_t io_conf = {};
+void config_output_pin(uint8_t gpio_pin)
+{
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask |= 1<<gpio_pin;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+}
+
---
+++ include/gpio_output.h
@@ -0,0 +1,2 @@
+#include "driver/gpio.h"
+void config_output_pin(uint8_t gpio_pin);
